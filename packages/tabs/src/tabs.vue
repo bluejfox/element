c0b605805cc5dcd866115e84c63bba88a001a242
bluejfox <bluejfox@gmail.com>
2018-05-31 17:43:05 +0800

    [Tabs]remove focus state when before-leave return a resolved promise

--- packages/tabs/src/tabs.vue
+++ packages/tabs/src/tabs.vue
@@ -86,6 +86,7 @@
           if (before && before.then) {
             before.then(() => {
               changeCurrentName();
+              this.$refs.nav && this.$refs.nav.removeFocus();
             });
           } else if (before !== false) {
             changeCurrentName();
