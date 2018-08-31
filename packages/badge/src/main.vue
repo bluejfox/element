3f94acae478c812995d6fff54e2f69547a6eff49
bluejfox <bluejfox@gmail.com>
2018-08-31 10:17:18 +0800

    [Badge]解决使用Badge的页面出现status参数错误警告的问题

--- packages/badge/src/main.vue
+++ packages/badge/src/main.vue
@@ -32,7 +32,7 @@ export default {
       type: String,
       validator(value) {
         // 这个值必须匹配下列字符串中的一个
-        return ['success', 'processing', 'default', 'error', 'warning'].indexOf(value) !== -1;
+        return ['success', 'processing', 'default', 'error', 'warning', ''].indexOf(value) !== -1;
       },
       default: ''
     },
