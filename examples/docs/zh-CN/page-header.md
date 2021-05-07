4a9d40c2726bbdaa9d94711c1f746bff81fa0aff
hanlei <13609850398@139.com>
2021-05-07 16:08:33 +0800

    [PageHeader]新增PageHeader

--- examples/docs/zh-CN/page-header.md
+++ examples/docs/zh-CN/page-header.md
@@ -1,19 +1,104 @@
 ## PageHeader 页头
 
-如果页面的路径比较简单，推荐使用页头组件而非面包屑组件。
+页头起到了内容概览和引导页级操作的作用。包括由面包屑、标题、页面内容简介、页面级导航组成。
 
-### 基础
+### 何时使用
+
+当需要使用户快速理解当前页是什么以及方便用户使用页面功能时使用，通常也可被用作页面间导航。
+
+### 简单示例
+
+标准页头，适合使用在需要简单描述的场景。
+
 
 :::demo
 ```html
-<el-page-header @back="goBack" content="详情页面">
-</el-page-header>
+<template>
+  <sr-page-header :bread-crumb="breadCrumb"
+                  title="页头">
+  </sr-page-header>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      breadCrumb: [
+        {
+          path: '#',
+          label: '第一级页面'
+        },
+        {
+          label: '第二级页面'
+        },
+        {
+          path: '#',
+          label: '当前页面'
+        }
+      ],
+    };
+  }
+}
+</script>
+```
+:::
 
+### 组合示例
+
+使用了 PageHeader 提供的所有能力。
+
+:::demo
+```html
+<template>
+  <sr-page-header :bread-crumb="breadCrumb"
+                  title="页头"
+                  :tab-list="tabList"
+                  :tab-active-key.sync="tabActiveKey">
+    <template slot="action">
+      <el-button type="primary">主操作</el-button>
+      <el-button>普通操作</el-button>
+      <el-button>普通操作</el-button>
+    </template>
+    <template slot="content">
+      <div>
+        大江东去，浪淘尽，千古风流人物。故垒西边，人道是：三国周郎赤壁。乱石穿空，惊涛拍岸，卷起千堆雪。江山如画，一时多少豪杰。
+        <br/>
+        遥想公瑾当年，小乔初嫁了，雄姿英发。羽扇纶巾，谈笑间、樯橹灰飞烟灭。故国神游，多情应笑我，早生华发。人生如梦，一尊还酹江月。
+      </div>
+    </template>
+    <template slot="extraContent">
+      <img src="https://gw.alipayobjects.com/zos/antfincdn/K%24NnlsB%26hz/pageHeader.svg" width="100%"/>
+    </template>
+  </sr-page-header>
+  {{ tabActiveKey }}
+</template>
 <script>
   export default {
-    methods: {
-      goBack() {
-        console.log('go back');
+    data() {
+      return {
+        breadCrumb: [
+          {
+            path: '#',
+            label: '第一级页面'
+          },
+          {
+            label: '第二级页面'
+          },
+          {
+            path: '#',
+            label: '当前页面'
+          }
+        ],
+        tabList: [
+          {
+            key: 'A',
+            label: '页签一'
+          },
+          {
+            key: 'B',
+            label: '页签二'
+          }
+        ],
+        tabActiveKey: 'B',
       }
     }
   }
@@ -22,19 +107,28 @@
 :::
 
 ### Attributes
-| 参数      | 说明          | 类型      | 可选值                           | 默认值  |
-|---------- |-------------- |---------- |------------------------------ | ------ |
-| title     | 标题           | string    |  —                            | 返回   |
-| content   | 内容           | string    |  —                            | —      |
 
+| 参数      | 说明          | 类型      | 可选值                           | 默认值  |
+|---------- |-------------- |---------- |--------------------------------  |-------- |
+| title     | 标题           | string | — | — |
+| content     | 内容           | string | — | — |
+| bread-crumb     | 面包屑列表，结构为{ path: string, label: string }           | array | — | — |
+| show-bread-crumb     | 是否显示面包屑           | array | — | — |
+| tab-list     | 标签页列表，结构为{ key: string, label: string }           | array | — | — |
+| tab-active-key     | 处于激活状态标签的key值          | string | — | — |
+
+### Slot
+
+| Name | Description |
+|------|--------|
+| 插槽名称 | 描述 |
+| title | 标题的内容 |
+| action | 操作按钮 |
+| content |  内容 |
+| extraContent | 额外内容 |
 
 ### Events
-| 事件名称   | 说明           | 回调参数   |
-|---------- |-------------- |---------- |
-| back      | 点击左侧区域触发 | —        |
 
-### Slots
-| 事件名称    | 说明         |
-|---------- |------------- |
-| title     | 标题内容      |
-| content   | 内容         |
+| 事件名称 | 说明 | 回调参数 |
+|---------- |-------- |---------- |
+| tab-click | 标签点击的事件 | 当前点击的标签对象 |
--- packages/page-header/src/main.vue
+++ packages/page-header/src/main.vue
@@ -1,30 +1,136 @@
 <template>
-  <div class="el-page-header">
-    <div class="el-page-header__left" @click="$emit('back')">
-      <i class="el-icon-back"></i>
-      <div class="el-page-header__title">
-        <slot name="title">{{ title }}</slot>
+  <el-card
+    shadow="never"
+    class="el-page-header"
+    :body-style="{
+      padding: 0,
+    }"
+  >
+    <!-- 面包屑 -->
+    <el-breadcrumb separator="/"
+                   v-if="showBreadCrumb && innerBreadCrumb && innerBreadCrumb.length > 0">
+      <el-breadcrumb-item
+        v-for="crumb in innerBreadCrumb"
+        :to="crumb.path ? { path: crumb.path } : undefined"
+        :key="`${crumb.label}-${crumb.path}`"
+      >
+        {{ crumb.label }}
+      </el-breadcrumb-item>
+    </el-breadcrumb>
+    <div class="detail">
+      <div class="logo" v-if="$slots.logo">
+        <slot name="logo"></slot>
+      </div>
+      <div class="main">
+        <div class="row">
+          <h1 class="title" v-if="currentTitle || $slots.title">
+            <template v-if="currentTitle">
+              {{ currentTitle }}
+            </template>
+            <slot name="title" v-else></slot>
+          </h1>
+          <div class="action" v-if="$slots.action">
+            <slot name="action"></slot>
+          </div>
+        </div>
+        <div class="row" v-if="content || $slots.content || $slots.extraContent">
+          <div class="el-page-header__content" v-if="content || $slots.content">
+            <template v-if="content">
+              {{ content }}
+            </template>
+            <slot name="content" v-else></slot>
+          </div>
+          <div class="extra-content" v-if="$slots.extraContent">
+            <slot name="extraContent"></slot>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="el-page-header__content">
-      <slot name="content">{{ content }}</slot>
+    <div class="tabs" v-if="tabList">
+      <el-tabs v-model="nestTabActiveKey" @tab-click="handleTabClick">
+        <el-tab-pane
+          v-for="tab in tabList"
+          :key="tab.key"
+          :name="tab.key"
+          :label="tab.label"
+        ></el-tab-pane>
+      </el-tabs>
     </div>
-  </div>
+  </el-card>
 </template>
-
 <script>
-import { t } from 'setaria-ui/src/locale';
-export default {
-  name: 'ElPageHeader',
+import { getValueByPath, isEmpty } from 'setaria-ui/src/utils/util';
 
+export default {
+  name: 'SrPageHeader',
   props: {
-    title: {
-      type: String,
+    routes: {
+      type: Array,
+      // 默认使用Vue实例上的$route对象
       default() {
-        return t('el.pageHeader.title');
+        return this.$route.matched;
       }
     },
-    content: String
+    breadCrumb: Array,
+    showBreadCrumb: {
+      type: Boolean,
+      default: true
+    },
+    title: String,
+    content: String,
+    tabList: Array,
+    tabActiveKey: null
+  },
+  data() {
+    return {
+      nestTabActiveKey: null,
+      innerBreadCrumb: []
+    };
+  },
+  computed: {
+    currentTitle() {
+      const { title } = this;
+      let ret = title;
+      if (isEmpty(title)) {
+        ret = getValueByPath(this.$route, 'meta.title') || '';
+      }
+      return ret;
+    }
+  },
+  watch: {
+    tabActiveKey: {
+      immediate: true,
+      handler(val) {
+        this.nestTabActiveKey = val;
+      }
+    },
+    nestTabActiveKey: {
+      immediate: true,
+      handler(val) {
+        this.$emit('update:tabActiveKey', val);
+      }
+    },
+    breadCrumb: {
+      immediate: true,
+      handler(val) {
+        this.innerBreadCrumb = val;
+      }
+    }
+  },
+  methods: {
+    /**
+     * Tab点击事件处理
+     * @event
+     * @param tabCompoent 被点击的Tab标签页的实例
+     * @param evt Event事件对象
+     */
+    handleTabClick(tabComponent, evt) {
+      // 保证先刷新tabActiveKey的值，再触发tab-click事件
+      this.$nextTick(() => {
+        const tab = this.tabList.find((item) => item.key === this.nestTabActiveKey);
+        this.$emit('tab-click', tab, evt);
+      });
+    }
   }
 };
 </script>
